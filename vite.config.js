db04250dfbff1533e3cafcb6f6b874c9820a2171
sanyxmm <iamsanyamchoudhary@gmail.com>
2025-03-30 18:27:16 +0530

    code updated

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,9 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
+  resolve: {
+    alias: {
+      '@mui/styled-engine': '@mui/styled-engine-sc',
     },
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
-    },
-  },
-  server: {
-    compress: false, // Disable compression to test
   },
 });
