3373f1e82cafff87f8a108bdfaeade17c508f50a
sanyxmm <iamsanyamchoudhary@gmail.com>
2025-03-30 18:13:24 +0530

    updated vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@mui/styled-engine': '@mui/styled-engine-sc',
+  plugins: [react()],
+  build: {
+    minify: 'terser', // Ensures minification
+    terserOptions: {
+      compress: {
+        drop_console: true, // Remove console logs
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Split vendor code
+          }
+        },
+      },
     },
   },
 });
